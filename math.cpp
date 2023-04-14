63430e587b8ebf64b658cfa1ebcad8330a2c58d5
AntoninLdn <antonin.laudon@epitech.eu>
2023-04-14 10:25:33 +0200

    ADD: math

---
+++ math.cpp
@@ -0,0 +1,215 @@
+/*
+** EPITECH PROJECT, 2023
+** B-OOP-400-TLS-4-1-bsraytracer-antonin.laudon
+** File description:
+** math
+*/
+
+#include "math.hpp"
+
+Math::Vector3D::Vector3D(double x, double y, double z) : _x(x), _y(y), _z(z)
+{
+}
+
+Math::Vector3D::~Vector3D()
+{
+}
+
+Math::Point3D::Point3D(double x, double y, double z) : _x(x), _y(y), _z(z)
+{
+}
+
+Math::Point3D::~Point3D()
+{
+}
+
+Math::Ray::Ray(Math::Point3D origin, Math::Vector3D direction) : _origin(origin), _direction(direction)
+{
+}
+
+Math::Ray::~Ray()
+{
+}
+
+double Math::Vector3D::getX() const
+{
+    return _x;
+}
+
+double Math::Vector3D::getY() const
+{
+    return _y;
+}
+
+double Math::Vector3D::getZ() const
+{
+    return _z;
+}
+
+double Math::Vector3D::setX(double x)
+{
+    _x = x;
+}
+
+double Math::Vector3D::setY(double y)
+{
+    _y = y;
+}
+
+double Math::Vector3D::setZ(double z)
+{
+    _z = z;
+}
+
+double Math::Point3D::getX() const
+{
+    return _x;
+}
+
+double Math::Point3D::getY() const
+{
+    return _y;
+}
+
+double Math::Point3D::getZ() const
+{
+    return _z;
+}
+
+double Math::Point3D::setX(double x)
+{
+    _x = x;
+}
+
+double Math::Point3D::setY(double y)
+{
+    _y = y;
+}
+
+double Math::Point3D::setZ(double z)
+{
+    _z = z;
+}
+
+Math::Point3D Math::Ray::getOrigin() const
+{
+    return _origin;
+}
+
+Math::Vector3D Math::Ray::getDirection() const
+{
+    return _direction;
+}
+
+Math::Point3D Math::Ray::setOrigin(Math::Point3D &origin)
+{
+    _origin = origin;
+}
+
+Math::Vector3D Math::Ray::setDirection(Math::Vector3D &direction)
+{
+    _direction = direction;
+}
+
+Math::Vector3D operator+(const Math::Vector3D &v1, const Math::Vector3D &v2)
+{
+    return Math::Vector3D(v1.getX() + v2.getX(), v1.getY() + v2.getY(), v1.getZ() + v2.getZ());
+}
+Math::Vector3D operator+(const Math::Vector3D &v1, double scalar)
+{
+    return Math::Vector3D(v1.getX() + scalar, v1.getY() + scalar, v1.getZ() + scalar);
+}
+Math::Vector3D operator+(double scalar, const Math::Vector3D &v1)
+{
+    return Math::Vector3D(v1.getX() + scalar, v1.getY() + scalar, v1.getZ() + scalar);
+}
+
+Math::Vector3D operator-(const Math::Vector3D &v1, const Math::Vector3D &v2)
+{
+    return Math::Vector3D(v1.getX() - v2.getX(), v1.getY() - v2.getY(), v1.getZ() - v2.getZ());
+}
+Math::Vector3D operator-(const Math::Vector3D &v1, double scalar)
+{
+    return Math::Vector3D(v1.getX() - scalar, v1.getY() - scalar, v1.getZ() - scalar);
+}
+Math::Vector3D operator-(double scalar, const Math::Vector3D &v1)
+{
+    return Math::Vector3D(scalar - v1.getX(), scalar - v1.getY(), scalar - v1.getZ());
+}
+
+Math::Vector3D operator*(const Math::Vector3D &v1, const Math::Vector3D &v2)
+{
+    return Math::Vector3D(v1.getX() * v2.getX(), v1.getY() * v2.getY(), v1.getZ() * v2.getZ());
+}
+Math::Vector3D operator*(const Math::Vector3D &v1, double scalar)
+{
+    return Math::Vector3D(v1.getX() * scalar, v1.getY() * scalar, v1.getZ() * scalar);
+}
+Math::Vector3D operator*(double scalar, const Math::Vector3D &v1)
+{
+    return Math::Vector3D(v1.getX() * scalar, v1.getY() * scalar, v1.getZ() * scalar);
+}
+
+Math::Vector3D operator/(const Math::Vector3D &v1, const Math::Vector3D &v2)
+{
+    return Math::Vector3D(v1.getX() / v2.getX(), v1.getY() / v2.getY(), v1.getZ() / v2.getZ());
+}
+Math::Vector3D operator/(const Math::Vector3D &v1, double scalar)
+{
+    return Math::Vector3D(v1.getX() / scalar, v1.getY() / scalar, v1.getZ() / scalar);
+}
+
+void operator+=(Math::Vector3D &v1, Math::Vector3D &v2)
+{
+    v1.setX(v1.getX() + v2.getX());
+    v1.setY(v1.getY() + v2.getY());
+    v1.setZ(v1.getZ() + v2.getZ());
+}
+void operator+=(Math::Vector3D &v1, double scalar)
+{
+    v1.setX(v1.getX() + scalar);
+    v1.setY(v1.getY() + scalar);
+    v1.setZ(v1.getZ() + scalar);
+}
+
+void operator-=(Math::Vector3D &v1, Math::Vector3D &v2)
+{
+    v1.setX(v1.getX() - v2.getX());
+    v1.setY(v1.getY() - v2.getY());
+    v1.setZ(v1.getZ() - v2.getZ());
+}
+void operator-=(Math::Vector3D &v1, double scalar)
+{
+    v1.setX(v1.getX() - scalar);
+    v1.setY(v1.getY() - scalar);
+    v1.setZ(v1.getZ() - scalar);
+}
+
+void operator*=(Math::Vector3D &v1, Math::Vector3D &v2)
+{
+    v1.setX(v1.getX() * v2.getX());
+    v1.setY(v1.getY() * v2.getY());
+    v1.setZ(v1.getZ() * v2.getZ());
+}
+void operator*=(Math::Vector3D &v1, double scalar)
+{
+    v1.setX(v1.getX() * scalar);
+    v1.setY(v1.getY() * scalar);
+    v1.setZ(v1.getZ() * scalar);
+}
+
+void operator/=(Math::Vector3D &v1, Math::Vector3D &v2)
+{
+    v1.setX(v1.getX() / v2.getX());
+    v1.setY(v1.getY() / v2.getY());
+    v1.setZ(v1.getZ() / v2.getZ());
+}
+void operator/=(Math::Vector3D &v1, double scalar)
+{
+    v1.setX(v1.getX() / scalar);
+    v1.setY(v1.getY() / scalar);
+    v1.setZ(v1.getZ() / scalar);
+}
+
+
+
---
+++ math.hpp
@@ -0,0 +1,88 @@
+/*
+** EPITECH PROJECT, 2023
+** B-OOP-400-TLS-4-1-bsraytracer-antonin.laudon
+** File description:
+** math
+*/
+
+#ifndef MATH_HPP_
+#define MATH_HPP_
+
+namespace Math {
+    class Vector3D {
+        public:
+            Vector3D(double x = 0, double y = 0, double z = 0);
+            ~Vector3D();
+            double getX() const;
+            double getY() const;
+            double getZ() const;
+            double setX(double x);
+            double setY(double y);
+            double setZ(double z);
+        private:
+            double _x;
+            double _y;
+            double _z;
+        protected:
+    };
+
+    class Point3D {
+        public:
+            Point3D(double x = 0, double y = 0, double z = 0);
+            ~Point3D();
+            double getX() const;
+            double getY() const;
+            double getZ() const;
+            double setX(double x);
+            double setY(double y);
+            double setZ(double z);
+        private:
+            double _x;
+            double _y;
+            double _z;
+        protected:
+    };
+
+    class Ray {
+        public:
+            Ray(Point3D origin, Vector3D direction);
+            ~Ray();
+            Point3D getOrigin() const;
+            Vector3D getDirection() const;
+            Point3D setOrigin(Point3D &origin);
+            Vector3D setDirection(Vector3D &direction);
+        private:
+            Point3D _origin;
+            Vector3D _direction;
+        protected:
+    };
+
+}
+Math::Vector3D operator+(const Math::Vector3D &v1, const Math::Vector3D &v2);
+Math::Vector3D operator+(const Math::Vector3D &v1, const double &d);
+Math::Vector3D operator+(const double &d, const Math::Vector3D &v1);
+
+Math::Vector3D operator-(const Math::Vector3D &v1, const Math::Vector3D &v2);
+Math::Vector3D operator-(const Math::Vector3D &v1, const double &d);
+Math::Vector3D operator-(const double &d, const Math::Vector3D &v1);
+
+Math::Vector3D operator*(const Math::Vector3D &v1, const Math::Vector3D &v2);
+Math::Vector3D operator*(const Math::Vector3D &v1, const double &d);
+Math::Vector3D operator*(const double &d, const Math::Vector3D &v1);
+
+Math::Vector3D operator/(const Math::Vector3D &v1, const Math::Vector3D &v2);
+Math::Vector3D operator/(const Math::Vector3D &v1, const double &d);
+
+void operator+=(Math::Vector3D &v1, const Math::Vector3D &v2);
+void operator+=(Math::Vector3D &v1, const double &d);
+
+void operator-=(Math::Vector3D &v1, const Math::Vector3D &v2);
+void operator-=(Math::Vector3D &v1, const double &d);
+
+void operator*=(Math::Vector3D &v1, const Math::Vector3D &v2);
+void operator*=(Math::Vector3D &v1, const double &d);
+
+void operator/=(Math::Vector3D &v1, const Math::Vector3D &v2);
+void operator/=(Math::Vector3D &v1, const double &d);
+
+#endif /* !MATH_HPP_ */
