a89f264f3f77afc1b80ae746111a0836af5d8a2a
ArtigasChristopher <christopher.artigas-fuentes@epitech.eu>
2023-04-24 14:36:01 +0200

    Replace Makefile by Cmake.

---
+++ CMakeFiles/raytracer.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "\$(PROJECT_SOURCE_DIR)/raytracer"
+  "CMakeFiles/raytracer.dir/src/Core/Core.cpp.o"
+  "CMakeFiles/raytracer.dir/src/Core/Core.cpp.o.d"
+  "CMakeFiles/raytracer.dir/src/PPM_generator/Generation.cpp.o"
+  "CMakeFiles/raytracer.dir/src/PPM_generator/Generation.cpp.o.d"
+  "CMakeFiles/raytracer.dir/src/Scene/Scene.cpp.o"
+  "CMakeFiles/raytracer.dir/src/Scene/Scene.cpp.o.d"
+  "raytracer"
+  "raytracer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raytracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/PPM_generator/Generation.hpp
+++ src/PPM_generator/Generation.hpp
@@ -19,19 +19,76 @@ namespace PPM {
     };
     class PPM {
         public:
+            /**
+             * @brief Construct a new PPM object
+             * 
+             * @param width 
+             * @param height 
+             */
             PPM(int width, int height);
+            /**
+             * @brief Destroy the PPM object
+             * 
+             */
             ~PPM();
+            /**
+             * @brief Set the Version object
+             * 
+             * @param version 
+             */
             void setVersion(std::string version);
+            /**
+             * @brief Set the Pixel object
+             * 
+             * @param x 
+             * @param y 
+             * @param color 
+             */
             void setPixel(int x, int y, RGB color);
+            /**
+             * @brief Transform a buffer of RGB into an in _pixels.
+             * 
+             * @param pixels 
+             */
             void bufferToImage(const RGB **pixels);
+            /**
+             * @brief Save the image in _pixels into a file.
+             * 
+             * @param filename 
+             */
             void save(const char *filename);
+            /**
+             * @brief clear the _pixels buffer.
+             * 
+             */
             void clear();
+            /**
+             * @brief Fill the _pixels buffer with a color.
+             * 
+             * @param color 
+             */
             void fill(RGB color);
         protected:
         private:
+            /**
+             * @brief Width of the image.
+             * 
+             */
             int _width;
+            /**
+             * @brief Height of the image.
+             * 
+             */
             int _height;
+            /**
+             * @brief Version of the PPM file.
+             * 
+             */
             std::string _version;
+            /**
+             * @brief The pixels of the final image.
+             * 
+             */
             RGB *_pixels;
     };
 }
