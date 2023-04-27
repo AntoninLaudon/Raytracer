929a20f35b6e4a2243de87e828459f8ba6f6b644
Yom <tom.laiolo@epitech.eu>
2023-04-27 13:59:43 +0200

    ADD: sphere generation

--- src/main.cpp
+++ src/main.cpp
@@ -16,18 +16,22 @@
 int main ()
 {
     Raytracer::Camera cam (Math::Point3D (0, 0, 0), Raytracer::Rectangle3d (Math::Point3D (0, 0, 1), Math::Vector3D (1, 0, 0), Math::Vector3D (0, 1, 0)));
-    Math::Sphere s (Math::Point3D (1, 1, 2), 0.5);
-    for (double y = 0; y < 100; y++) {
-        for (double x = 0; x < 100; x++) {
-            double u = x/100;
-            double v = y/100;
+    Math::Sphere s (Math::Point3D (0, 0, 2), 0.5);
+    Math::Sphere s2 (Math::Point3D (7, 5, 7), 1);
+    std::cout << "P3" << std::endl;
+    std::cout << "4000 4000 255" << std::endl;
+    for (double y = 4000; y > 0; y--) {
+        for (double x = 0; x < 4000; x++) {
+            double u = x/4000;
+            double v = y/4000;
             Math::Ray r = cam.ray (u, v);
             if (s.hits (r)) {
-                std::cout << "X";
+                std::cout << "255 0 0" << std::endl;
+            } else if (s2.hits (r)) {
+                std::cout << "0 0 255" << std::endl;
             } else {
-                std::cout << " ";
+                std::cout << "0 0 0" << std::endl;
             }
         }
-        std::cout << std::endl;
     }
 }
